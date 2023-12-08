6b0ae07e9a1edb179b47d2a97a5c3f54e132e772
khailuu <khailuu123@gmail.com>
2023-12-08 19:26:20 +0700

    updateLogin

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
 const currentTheme = localStorage.getItem("theme");
-const logo = document.querySelector('.change__logo'); // Corrected selector
+const logo = document.querySelector(".change__logo"); // Corrected selector
 
 if (currentTheme) {
   document.documentElement.setAttribute("data-theme", currentTheme);
@@ -49,11 +51,7 @@ getEle(".nav-menu").onclick = () => {
   overlay.classList.toggle("active");
 };
 
-getEle(".overlay__menu").onclick = () => {
-  const menu = getEle(".header__nav");
-  menu.classList.toggle("active");
-  getEle(".overlay__menu.active").classList.remove("active");
-};
+
 
 const btnFooters = document.querySelectorAll(".btn-footer");
 const footerLinks = document.querySelectorAll(".footer__link");
@@ -91,4 +89,113 @@ getEle("#btn__product").onclick = () => {
 
   iconNav.classList.toggle("active");
   menuProduct.classList.toggle("active");
+};
+
+// btntotop
+mybutton = document.getElementById("myBtn");
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+const wrapper = document.querySelector(".wrapper");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
+const btnPopup = document.querySelector(".btnLogin-popup");
+const btnPopup1 = document.querySelector(".btnLogin-popup-1");
+const btnClose = document.querySelector(".icon-close");
+const btnMenu = document.getElementById("btn-menu");
+const popUpMenu = document.querySelector(".popup-menu");
+const closeMenu = document.querySelector(".icon-close-menu");
+const overlay = getEle(".overlay__menu");
+if (btnMenu) {
+  btnMenu.addEventListener("click", () => {
+    closeMenu.classList.add("active-close");
+  });
+}
+
+if (btnMenu) {
+  btnMenu.addEventListener("click", () => {
+    btnMenu.classList.add("active-hidden");
+    closeMenu.classList.remove("display-bars");
+  });
+}
+
+if (closeMenu) {
+  closeMenu.addEventListener("click", () => {
+    popUpMenu.classList.remove("active-menu");
+    btnMenu.classList.remove("active-hidden");
+    closeMenu.classList.remove("active-close");
+    closeMenu.classList.add("display-bars");
+  });
+}
+
+if (btnMenu) {
+  btnMenu.addEventListener("click", () => {
+    popUpMenu.classList.add("active-menu");
+  });
+}
+
+// if(btnMenu) {
+//     btnMenu.addEventListener('click', ()=> {
+//         popUpMenu.classList.remove('active-menu');
+//     })
+// }
+
+if (btnPopup) {
+  btnPopup.addEventListener("click", () => {
+    wrapper.classList.add("active-popup");
+    
+    overlay.classList.add("active");
+    const menu = getEle(".header__nav");
+    menu.classList.remove("active");
+  });
+}
+
+if (btnPopup1) {
+  btnPopup1.addEventListener("click", () => {
+    wrapper.classList.add("active-popup");
+    
+    overlay.classList.add("active");
+    const menu = getEle(".header__nav");
+    menu.classList.remove("active");
+  });
+}
+
+if (btnClose) {
+  btnClose.addEventListener("click", () => {
+    wrapper.classList.remove("active-popup");
+    overlay.classList.remove("active");
+    const menu = getEle(".header__nav");
+    menu.classList.remove("active");
+  });
+}
+
+if (registerLink) {
+  registerLink.addEventListener("click", () => {
+    wrapper.classList.add("active");
+  });
+}
+
+if (loginLink) {
+  loginLink.addEventListener("click", () => {
+    wrapper.classList.remove("active");
+  });
+}
+
+getEle(".overlay__menu").onclick = () => {
+  const menu = getEle(".header__nav");
+  getEle(".overlay__menu.active").classList.remove("active");
+  wrapper.classList.remove("active-popup");
+    menu.classList.remove("active");
 };
